3a55724ebeeda5b4ef3fc9db157318f7425cdefd
Shahensha <64788000+mr-shahensha@users.noreply.github.com>
2023-08-12 11:54:55 +0530

    Income & Expenditure done

--- inex.php
+++ inex.php
@@ -107,170 +107,16 @@ function searchForId($id, $array,$chkfld,$sendfld) {
               </div>
               <div class="form-group col-md-12" align="right">
                 <br>
-                <input type="hidden" value="<?php echo $sl; ?>" id="sl">
-                <input type="hidden" value="<?php echo $values['pid'];?>" id="pid">
                 <input type="button" class="btn btn-success" value="SEARCH" onclick="cldt()">
               </div>
             </div>
+            <!-- ***Show Data Start*** -->
 <div class="form-group col-md-12">
-<table class="table table-bordered">
-      <thead>
-    <tr>
-      <th scope="col">Income</th>
-      <th scope="col">Expenditure</th>
+<div id="showdata">
 
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td><table class="table table-striped">
-      <thead>
-    <tr>
-      <th scope="col">SL</th>
-      <th scope="col">Income Head</th>
-      <th scope="col">Date</th>
-      <th scope="col">Balance</th>
-
-    </tr>
-  </thead>
-  <tbody>
-  <?php
-			
-      $fld1['pid']='3';
-      $op1['pid']="=,";
-
-      $list1  = new Init_Table();
-      $list1->set_table("main_ledger","sl");
-      $row=$list1->search_custom($fld1,$op1,'',array('sl' => 'ASC'));
-      $pdo= new MainPDO();
-      $f=0;
-    $total=0;
-      foreach ($row as $values) 
-      {    
-                 $f++;
-
- 
-			?>
-            <tr>
-<td><?php echo $f;?></td>
-<td><a href="incex_heads.php?sl=<?php echo base64_encode($values['sl']);?>" ><?php echo $values['ldg'];?></a> </td>
-<?php
-$fld1y['cr'] = $values['sl'];
-$op1y['cr'] = "=, ";
-$newamty=0;
-$main_drcry  = new Init_Table();
-$main_drcry->set_table("main_drcr","sl");
-$fldy = array("SUM(amt) as newamt ");
-$rowy = $main_drcry->search_custom_ultra($fld1y, $op1y, '', array('sl' => 'ASC'), $fldy);
-foreach ($rowy as $resy) {
-	$credit = $resy['newamt'];
-}
-
-
-?>
-<td><?php 
-$fld10['cr'] = $values['sl'];
-$op10['cr'] = "=, ";
-      $main_drcr  = new Init_Table();
-      $main_drcr->set_table("main_drcr","sl");
-      $row=$main_drcr->search_custom($fld10,$op10,'',array('sl' => 'ASC'));
-      $pdo= new MainPDO();
-
-      foreach ($row as $values) 
-      { 
-
-      }
-      echo $values['edt'];
-
-      ?></td>
-
-<td><?php echo $credit;?></td>
-
-</tr>
-<?php
-                      $total=$total+$credit;
-
-      }
-?>
-<th><td> <b>Total :<?php echo $total;?> </b> </td></th>
-
-  </tbody>
-</table></td>
-      <td><table class="table table-striped">
-      <thead>
-    <tr>
-    <th scope="col">SL</th>
-      <th scope="col">Expenditure Head</th>
-      <th scope="col">Date</th>
-
-      <th scope="col">Balance</th>
-    </tr>
-  </thead>
-  <tbody>
-  <?php
-			
-            $fld1['pid']='4';
-            $op1['pid']="=,";
-            $total2=0;
-            $list1  = new Init_Table();
-            $list1->set_table("main_ledger","sl");
-            $row=$list1->search_custom($fld1,$op1,'',array('sl' => 'ASC'));
-            $pdo= new MainPDO();
-            $t=0;
-            $total=0;
-            foreach ($row as $values) 
-            {    
-                       $t++;
-                
-       
-                  ?>
-                  <tr>
-      <td><?php echo $t;?></td>
-      <td><a href="incex_heads.php?sl=<?php echo base64_encode($values['sl']);?>"> <?php echo $values['ldg'];?></a></td>
-      <?php
-
-      
-      $fld110['dr'] = $values['sl'];
-      $op110['dr'] = "=, ";
-      $newamt0=0;
-      $main_drcr0  = new Init_Table();
-      $main_drcr0->set_table("main_drcr","sl");
-      $fld10 = array("SUM(amt) as newamt0 ");
-      $row0 = $main_drcr0->search_custom_ultra($fld110, $op110, '', array('sl' => 'ASC'), $fld10);
-      foreach ($row0 as $res0) {
-          $debit = $res0['newamt0'];
-      }
-      ?>
-       <td><?php 
-$fld['cr'] = $values['sl'];
-$op['cr'] = "=, ";
-      $main_drcrx  = new Init_Table();
-      $main_drcrx->set_table("main_drcr","sl");
-      $r=$main_drcrx->search_custom($fld,$op,'',array('sl' => 'ASC'));
-      $pdo= new MainPDO();
-
-      foreach ($row as $value) 
-      { 
-
-      }
-      echo $value['edt'];
-            ?></td>
-      
-      <td><?php echo $debit;?></td>
-      </tr>
-      <?php
-             $total2=$total2+$debit;
-            }
-      ?>
-      <th><td><b>Total :<?php echo $total2;?> </b></td></th>
-
-  </tbody>
-</table></td>
-    </tr>
-
-  </tbody>
-</table>
 </div>
+</div>
+            <!-- ***Show Data End*** -->
 
 </div>
 
@@ -339,11 +185,13 @@ $op['cr'] = "=, ";
 </body>
 </html>
 <script>
+  $(document).ready(function(){
+  cldt();
+});
+
 function cldt() {
-                var pid = document.getElementById('pid').value; 
-                var sl = document.getElementById('sl').value;   
                 var d1 = document.getElementById('d1').value;
                 var d2 = document.getElementById('d2').value;
-                $('#showdata4').load('inex_head_data.php?d1=' + d1 + '&d2=' + d2 + '&sl=' + sl+'&pid='+pid).fadeIn('slow');
+                $('#showdata').load('inex_table.php?d1=' + d1 + '&d2=' + d2).fadeIn('slow');
               }
             </script>
